e98aa62225db8a8be2dd29952a66ff90612f5d15
Luis Sandoval <luissandoval1126@gmail.com>
2020-04-25 11:13:55 -0700

    added Compression

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var session = require("express-session");
+var compression = require("compression");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 //require dotenv so we can use enviorment variables
-require('dotenv').config();
-
+require("dotenv").config();
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
@@ -16,10 +16,13 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(compression());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -27,11 +30,13 @@ app.use(passport.session());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
-
